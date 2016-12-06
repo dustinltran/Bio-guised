67a273f13a4b22072db86bd350bba644ff0aa6ee
dustinltran <dustin_tran@live.com>
2016-12-05 21:30:35 -0800

    watchdog

--- main.cpp
+++ main.cpp
@@ -4,16 +4,29 @@
 #include "Windows.h"
 #include "biomodel.h"
 #include "bioguised.h"
+#include "thread"
 
 //BioGuised *bioGuised = new BioGuised();
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    std::thread watchdog(watchdog);
 
     MainWindow w;
     w.show();
+
+    std::thread watchdog(watchdog);
     BioModel* bioModel = new BioModel(); //Need for use of functions from biomodel
     ;
+
+
     return a.exec();
- }
+}
+
+void watchdog(){
+    QSerialPort *availableport;
+
+
+}
+
